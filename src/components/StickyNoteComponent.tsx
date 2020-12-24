93e7c1c93aba9950388567b0e46ff6177d4d1c64
XLpacman805 <xlpacman805@gmail.com>
2020-12-23 20:53:06 -0800

    added tdo in comments

--- src/components/StickyNoteComponent.tsx
+++ src/components/StickyNoteComponent.tsx
@@ -7,6 +7,14 @@ export class StickyNoteComponent extends React.Component<StickyNote, {}> {
         this.state = {};
     }
 
+    /**
+     * Need to write a handleChange method. 
+     * Need to replace lable and p with inputs so that a user can type. 
+     * The handleChange method should listen for changes. 
+     * After a 3 seconds of no changes, it should then write to the indexedDB. 
+     * I don't want to have a new transation on every keystroke. That would be expensive for a real db.
+     */
+
     render() {
         return (
             <div className="stickynote" id={this.props.id}>
